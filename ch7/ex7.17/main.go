554216e69ec0af6a8b12fa7b66ef1a15894ca38d
fanghao <fanghao90s@gmail.com>
2022-06-03 21:26:51 +0800

    update chapter 7

---
+++ ch7/ex7.17/main.go
@@ -0,0 +1,6 @@
+// @todo
+
+// Extend xmlselect so that elements may be selected not just by name, but by their
+// attributes too, in the manner of CSS, so that, for instance, an element like <div
+// id="page" class="wide"> could be selected by a matching id or class as well as
+// its name.
---
+++ ch7/ex7.18/main.go
@@ -0,0 +1,7 @@
+// @todo
+
+// Using the token-based decoder API, write a program that will readd an arbitrary
+// XML document and construct a tree of generic nodes that represents it. Nodes are
+// of two kinds: CharData nodes represent text strings, and Element nodes represent
+// named elements and their attributes. Each element node has a slice of child
+// nodes.
